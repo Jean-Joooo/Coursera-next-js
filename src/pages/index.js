ff63f4b4e043cc8f1f40295de0157355c89104be
Jean-JooosephA <jeanjosephagostini@outlook.com>
2024-03-07 14:27:39 +0100

    some changes

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,7 +3,7 @@ import Image from "next/image";
 import Link from 'next/link'
 import { Inter } from "next/font/google";
 import styles from "@/styles/Home.module.css";
-import Layout from "/components/Layout";
+import Layout from "@/components/Layout";
 
 const inter = Inter({ subsets: ["latin"] });
 
